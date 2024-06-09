2f7d977c87053a3b81e0dfd16b5aeaa04850a402
whes1015 <whes1015@gmail.com>
2024-06-10 03:46:45 +0800

    feat: tsunami

--- src/js/tsunami.js
+++ src/js/tsunami.js
@@ -1,3 +1,21 @@
-function show_tsunami(data) {
+/* eslint-disable no-undef */
+setInterval(() => show_tsunami(), 3000);
 
+function show_tsunami(data) {
+  variable.tsunami_geojson = L.geoJson.vt(require(path.join(__dirname, "../resource/map", "tsunami.json")), {
+    minZoom : 4,
+    maxZoom : 12,
+    buffer  : 256,
+    zIndex  : 5,
+    style   : (args) => {
+      const name = args.COUNTYNAME + " " + args.TOWNNAME;
+      return {
+        color       : "red",
+        weight      : 5,
+        fillColor   : "transparent",
+        fillOpacity : 0,
+      };
+    },
+  }).addTo(variable.map);
+  setTimeout(() => variable.tsunami_geojson.remove(), 2000);
 }
