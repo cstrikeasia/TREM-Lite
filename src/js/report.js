007376555667433b1f37f39d13e996ccd955981e
Miyashooooo <cstrike.asia@gmail.com>
2024-06-11 00:08:31 +0800

    perf: report and setting

--- src/js/report.js
+++ src/js/report.js
@@ -22,16 +22,23 @@ const InfoBodyTitleBox = querySelector(".info-body-title-box");
 const InfoBodyFooter = querySelector(".info-body-footer");
 const ReportIntensityGrouped = querySelector("#report-intensity-grouped");
 
-let Report_DATA = {};
-let last_report = {};
-const List_maxRetries = 3;
+function createLocationElement(textContent, className, bgText, html, attr) {
+  const element = createElement("div");
+  element.classList = className;
+  element.textContent = textContent;
+  if (bgText) element.dataset.backgroundText = bgText;
+  if (html) element.innerHTML = html;
+  if (attr)
+    Object.entries(attr).forEach(([key, value]) => {
+      element.setAttribute(key, value);
+    });
+  return element;
+}
+
 async function report(retryCount = 0) {
-  let survey = null;
-  let check_ = 1;
+  let s = variable.report.survey;
   for (const int in variable.intensity_list)
-    survey = variable.intensity_list[int].data;
-
-
+    s = variable.intensity_list[int].data;
   try {
     logger.info("[Fetch] Fetching report data...");
     const ReportList = document.querySelector(".report-list-items");
@@ -39,77 +46,49 @@ async function report(retryCount = 0) {
     if (!res.ok) return;
     logger.info("[Fetch] Got report data");
     const data = await res.json();
-    Report_DATA = data;
     ReportList.innerHTML = "";
-    let WithoutNo = "";
-    if (survey) check_ = 0;
 
-    const FirstItem = survey ? survey : data[0];
-    const First = document.createElement("div");
+    if (s) variable.report.check_ = 0;
+    variable.report.data = data;
+
+    const FirstItem = s ? s : data[0];
+    const First = createElement("div");
     First.classList.add("report-list-item-index", "first");
-    First.setAttribute("data-report-id", survey ? "" : FirstItem.id);
-    if ((!last_report || JSON.stringify(last_report) !== JSON.stringify({ id: FirstItem.id })) && UserCheckBox()["sound-effects-Report"] == 1) {
-      last_report = { id: FirstItem.id };
+    First.setAttribute("data-report-id", s ? "" : FirstItem.id);
+
+    if ((!variable.report.last || JSON.stringify(variable.report.last) !== JSON.stringify({ id: FirstItem.id })) && UserCheckBox()["sound-effects-Report"] == 1) {
+      variable.report.last = { id: FirstItem.id };
       constant.AUDIO.REPORT.play();
     }
 
-    const No = survey ? "" : FirstItem.id.split("-");
-    const check_No = survey ? "" : No[0].split(3)[1];
+    const No = s ? "" : FirstItem.id.split("-");
+    const CheckNo = s ? "" : No[0].split(3)[1];
+    if (CheckNo == "000") variable.report.withoutNo = "Normal";
 
-    if (check_No == "000")
-      WithoutNo = "Normal";
-
-
-    const IntWrapper = document.createElement("div");
-    IntWrapper.classList.add("report-list-item-int-wrapper");
-
-    const Int = document.createElement("div");
-    Int.classList.add("report-list-item-int", `intensity-${survey ? survey.max : FirstItem.int}`);
-    Int.textContent = survey ? survey.max : FirstItem.int;
-
-    const IntTitle = document.createElement("div");
-    IntTitle.classList.add("report-list-item-int-title");
-    IntTitle.textContent = "觀測最大震度";
+    const IntWrapper = createLocationElement("", "report-list-item-int-wrapper");
+    const Int = createLocationElement(s ? s.max : FirstItem.int, `report-list-item-int intensity-${s ? s.max : FirstItem.int}`);
+    const IntTitle = createLocationElement("觀測最大震度", "report-list-item-int-title");
 
     IntWrapper.appendChild(Int);
     IntWrapper.appendChild(IntTitle);
     First.appendChild(IntWrapper);
 
-    let InfoWrapper = document.createElement("div");
-    InfoWrapper.classList.add("report-list-item-info-wrapper");
-
-    const Info = document.createElement("div");
-    Info.classList.add("report-list-item-info");
-
-    const Location = document.createElement("div");
-    Location.classList.add("report-list-item-location");
-    Location.textContent = survey ? "震源 調查中" : LocalReplace(FirstItem.loc);
-
-    const Time = document.createElement("div");
-    Time.classList.add("report-list-item-time");
-    Time.textContent = ReportTimeFormat(survey ? survey.id : FirstItem.time);
+    let InfoWrapper = createLocationElement("", "report-list-item-info-wrapper");
+    const Info = createLocationElement("", "report-list-item-info");
+    const Location = createLocationElement(s ? "震源 調查中" : LocalReplace(FirstItem.loc), "report-list-item-location");
+    const Time = createLocationElement(ReportTimeFormat(s ? s.id : FirstItem.time), "report-list-item-time");
 
     Info.appendChild(Location);
     Info.appendChild(Time);
     InfoWrapper.appendChild(Info);
 
-    if (!survey) {
-      const MagDepthWrapper = document.createElement("div");
-      MagDepthWrapper.classList.add("report-list-item-mag-depth");
-
-      const Mag = document.createElement("div");
-      Mag.classList.add("report-list-item-mag");
-      Mag.dataset.backgroundText = "規模";
-      Mag.innerHTML = `<div class="report-list-item-magnitude ${WithoutNo}">${FirstItem.mag < 10 ? FirstItem.mag.toFixed(1) : FirstItem.mag}</div>`;
-
-      const KM = document.createElement("div");
-      KM.classList.add("report-list-item-km");
-      KM.dataset.backgroundText = "深度";
-      KM.innerHTML = `<div class="km">${FirstItem.depth}</div>`;
+    if (!s) {
+      const MagDepthWrapper = createLocationElement("", "report-list-item-mag-depth");
+      const Mag = createLocationElement("", "report-list-item-mag", "規模", `<div class="report-list-item-magnitude ${variable.report.withoutNo}">${FirstItem.mag < 10 ? FirstItem.mag.toFixed(1) : FirstItem.mag}</div>`);
+      const KM = createLocationElement("", "report-list-item-km", "深度", `<div class="km">${FirstItem.depth}</div>`);
 
       MagDepthWrapper.appendChild(Mag);
       MagDepthWrapper.appendChild(KM);
-
       InfoWrapper.appendChild(MagDepthWrapper);
     }
 
@@ -118,64 +97,38 @@ async function report(retryCount = 0) {
     ReportList.appendChild(First);
 
     // 非First
-    for (let i = check_; i < data.length; i++) {
-      WithoutNo = "";
+    for (let i = variable.report.check_; i < data.length; i++) {
+      variable.report.withoutNo = "";
       const item = data[i];
 
       const No = item.id.split("-");
-      const check_No = No[0].split(3)[1];
+      const CheckNo = No[0].split(3)[1];
 
-      if (check_No == "000")
-        WithoutNo = "Normal";
+      if (CheckNo == "000") variable.report.withoutNo = "Normal";
 
-      const Element = document.createElement("div");
-      Element.classList.add("report-list-item-index");
-      Element.setAttribute("data-report-id", item.id);
+      const Element = createLocationElement("", "report-list-item-index", "", "", { "data-report-id": item.id });
+      const IntItem = createLocationElement(item.int, `report-list-item-int intensity-${item.int}`);
+      InfoWrapper = createLocationElement("", "report-list-item-info-wrapper");
 
-      const IntItem = document.createElement("div");
-      IntItem.classList.add("report-list-item-int", `intensity-${item.int}`);
-      IntItem.textContent = item.int;
-
-      InfoWrapper = document.createElement("div");
-      InfoWrapper.classList.add("report-list-item-info-wrapper");
-
-      const InfoItem = document.createElement("div");
-      InfoItem.classList.add("report-list-item-info");
-
-      const LocationItem = document.createElement("div");
-      LocationItem.classList.add("report-list-item-location");
-      LocationItem.textContent = LocalReplace(item.loc);
-
-      const TimeItem = document.createElement("div");
-      TimeItem.classList.add("report-list-item-time");
-      TimeItem.textContent = ReportTimeFormat(item.time);
-
-      const MagItem = document.createElement("div");
-      MagItem.classList.add("report-list-item-mag");
-      MagItem.textContent = `M ${item.mag < 10 ? item.mag.toFixed(1) : item.mag}`;
+      const InfoItem = createLocationElement("", "report-list-item-info");
+      const LocationItem = createLocationElement(LocalReplace(item.loc), "report-list-item-location");
+      const TimeItem = createLocationElement(ReportTimeFormat(item.time), "report-list-item-time");
 
       InfoItem.appendChild(LocationItem);
       InfoItem.appendChild(TimeItem);
 
-      MagDepthWrapper = document.createElement("div");
-      MagDepthWrapper.classList.add("report-list-item-mag-depth");
-
-      const MagDepth = document.createElement("div");
-      MagDepth.classList.add("report-list-item-mag", "report-list-item-mag-depth");
-      MagDepth.dataset.backgroundText = "規模";
-      MagDepth.innerHTML = `<div class="report-list-item-magnitude ${WithoutNo}">M ${item.mag < 10 ? item.mag.toFixed(1) : item.mag}</div>`;
+      MagDepthWrapper = createLocationElement("", "report-list-item-mag-depth");
+      const MagDepth = createLocationElement("", "report-list-item-mag report-list-item-mag-depth", "規模", `<div class="report-list-item-magnitude ${variable.report.withoutNo}">M ${item.mag < 10 ? item.mag.toFixed(1) : item.mag}</div>`);
 
       MagDepthWrapper.appendChild(MagDepth);
       InfoWrapper.appendChild(InfoItem);
       InfoWrapper.appendChild(MagDepthWrapper);
-
       Element.appendChild(IntItem);
       Element.appendChild(InfoWrapper);
-
       ReportList.appendChild(Element);
     }
   } catch (error) {
-    if (retryCount < List_maxRetries) {
+    if (retryCount < variable.report.list_retry) {
       logger.error(`[Fetch] ${error} (Try #${retryCount})`);
       await new Promise(resolve => setTimeout(resolve, 1000));
       await report(retryCount + 1);
@@ -197,7 +150,7 @@ async function ReportInfo(id, int, retryCount = 0) {
 
     const data = await res.json();
     const No = data.id.split("-")[0];
-    const check_No = No.split(3)[1];
+    const CheckNo = No.split(3)[1];
     open_DATA = data;
 
     display_element([ReportBoxWrapper], "flex");
@@ -212,7 +165,7 @@ async function ReportInfo(id, int, retryCount = 0) {
     ReportDepth.textContent = depth;
     ReportTime.textContent = formatTime(time).replace(/\//g, "-");
     ReportTitle.textContent = LocalReplace(loc);
-    ReportSubTitle.textContent = `${check_No !== "000" ? `編號 ${No}` : "小區域有感地震"}`;
+    ReportSubTitle.textContent = `${CheckNo !== "000" ? `編號 ${No}` : "小區域有感地震"}`;
     ReportMaxIntensity.className = `report-max-intensity intensity-${int}`;
     report_grouped(data);
     report_all(data);
@@ -236,20 +189,20 @@ function report_grouped(data) {
   cities.forEach(city => {
     const CityData = data.list[city];
 
-    const ReportListWrapper = document.createElement("div");
+    const ReportListWrapper = createElement("div");
     ReportListWrapper.classList.add("report-list-item-wrapper", "active");
 
-    const ReportList = document.createElement("div");
+    const ReportList = createElement("div");
     ReportList.classList.add("report-list-item");
 
-    const ReportListInt = document.createElement("div");
+    const ReportListInt = createElement("div");
     ReportListInt.classList.add("report-list-int");
 
-    const ReportIntensity = document.createElement("div");
+    const ReportIntensity = createElement("div");
     ReportIntensity.classList.add("report-intensity", `intensity-${CityData.int}`);
     ReportIntensity.textContent = CityData.int;
 
-    const ReportLoc = document.createElement("div");
+    const ReportLoc = createElement("div");
     ReportLoc.classList.add("report-location");
     ReportLoc.textContent = city;
 
@@ -257,7 +210,7 @@ function report_grouped(data) {
     ReportListInt.appendChild(ReportLoc);
     ReportList.appendChild(ReportListInt);
 
-    const ReportArrowDown = document.createElement("span");
+    const ReportArrowDown = createElement("span");
     ReportArrowDown.classList.add("report-arrow-down", "button-leading-icon", "material-symbols-rounded");
     ReportArrowDown.textContent = "keyboard_arrow_down";
 
@@ -265,24 +218,24 @@ function report_grouped(data) {
 
     ReportListWrapper.appendChild(ReportList);
 
-    const ReportInts = document.createElement("div");
+    const ReportInts = createElement("div");
     ReportInts.classList.add("report-int-items");
 
     const Towns = Object.keys(CityData.town);
 
     Towns.forEach(town => {
       const townData = CityData.town[town];
-      const ReportInt = document.createElement("div");
+      const ReportInt = createElement("div");
       ReportInt.classList.add("report-int-item");
 
-      const ReportIntInfo = document.createElement("div");
+      const ReportIntInfo = createElement("div");
       ReportIntInfo.classList.add("report-int-item-info");
 
-      const Int = document.createElement("div");
+      const Int = createElement("div");
       Int.classList.add("int", `intensity-${townData.int}`);
       Int.textContent = townData.int;
 
-      const Town = document.createElement("div");
+      const Town = createElement("div");
       Town.classList.add("town");
       Town.textContent = town;
 
@@ -302,14 +255,14 @@ function report_all(data) {
   reportContainer.innerHTML = "";
 
   Object.entries(data.list).forEach(([city, { int: cityIntensity }]) => {
-    const reportItem = document.createElement("div");
+    const reportItem = createElement("div");
     reportItem.classList.add("report-list-item");
 
-    const intensityDiv = document.createElement("div");
+    const intensityDiv = createElement("div");
     intensityDiv.classList.add("report-intensity", `intensity-${cityIntensity}`);
     intensityDiv.textContent = cityIntensity;
 
-    const locationDiv = document.createElement("div");
+    const locationDiv = createElement("div");
     locationDiv.classList.add("report-location");
     locationDiv.textContent = city;
 
@@ -379,7 +332,7 @@ ReportListBtn.addEventListener("click", function() {
 // 地震報告項目
 ReportItem.addEventListener("click", (event) => {
   const { dataset: { reportId: ReportID } } = event.target.closest(".report-list-item-index");
-  const ThisReport = Report_DATA.find(ReportInt => ReportInt.id == ReportID);
+  const ThisReport = variable.report.data.find(ReportInt => ReportInt.id == ReportID);
   if (ThisReport) ReportInfo(ReportID, ThisReport.int);
 });
 
